5889432e007e74ac4fd9c7c0dcfd6a81ecb4299f
Jcrosellon <119710206+Jcrosellon@users.noreply.github.com>
2024-05-04 00:14:49 -0500

    Nuevo cambio
    se crea el archivo Register.php en app/controller

---
+++ api_ADMIRED/app/Controllers/Register.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Controllers;
+
+use App\Controllers\BaseController; 
+use CodeIgniter\API\ResponseTrait;
+use App\Models\UserModel;
+
+class Register extends BaseController
+{
+    use ResponseTrait;
+
+    public function index()
+    {
+        $rules = [
+            'email' => [
+                'rules' => 'required|min_length[4]|max_length[255]|valid_email|is_unique[users.email]',
+                'label' => 'email'
+            ],
+            'password' => [
+                'rules' => 'required|min_length[8]|max_length[255]',
+                'label' => 'password',
+                'errors' => [
+                    'matches' => 'Password and confirm password do not match.'
+                ]
+            ],
+            'confirm_password' => [
+                'rules' => 'matches[password]',
+                'label' => 'confirm password'
+            ]
+        ];
+
+        if ($this->validate($rules)) {
+            $model = new UserModel();
+            $data = [
+                'email' => $this->request->getVar('email'),
+                'password' => password_hash($this->request->getVar('password'), PASSWORD_DEFAULT)
+            ];
+            $model->save($data);
+            return $this->respond(['message' => 'Registered Successfully'], 200);
+        } else {
+            $response = [
+                'errors' => $this->validator->getErrors(),
+                'message' => 'Invalid Inputs'
+            ];
+            return $this->fail($response, 409);
+        }
+    }
+}
