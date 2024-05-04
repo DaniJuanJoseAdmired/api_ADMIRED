bfb2a9c452a85fd1c037c20015846f626afd10f0
Jcrosellon <119710206+Jcrosellon@users.noreply.github.com>
2024-05-04 09:24:34 -0500

    Nuevo cambio
    Se ajusta el modelo UserModel.php

--- api_ADMIRED/app/Models/UserModel.php
+++ api_ADMIRED/app/Models/UserModel.php
@@ -6,26 +6,22 @@ use CodeIgniter\Model;
 
 class UserModel extends Model
 {
+    protected $DBGroup          = 'default';
     protected $table            = 'users';
     protected $primaryKey       = 'id';
     protected $useAutoIncrement = true;
     protected $returnType       = 'array';
     protected $useSoftDeletes   = false;
     protected $protectFields    = true;
-    protected $allowedFields    = [];
-
-    protected bool $allowEmptyInserts = false;
-    protected bool $updateOnlyChanged = true;
-
-    protected array $casts = [];
-    protected array $castHandlers = [];
+    protected $allowedFields    = ['email', 'password'];
 
     // Dates
-    protected $useTimestamps = false;
-    protected $dateFormat    = 'datetime';
-    protected $createdField  = 'created_at';
-    protected $updatedField  = 'updated_at';
-    protected $deletedField  = 'deleted_at';
+    protected $useTimestamps    = true;
+    protected $dateFormat       = 'datetime';
+    protected $createdField     = 'created_at';
+    protected $updatedField     = 'updated_at';
+    protected $deletedField     = 'deleted_at';
+
 
     // Validation
     protected $validationRules      = [];
